fbb7461879265992efcd2e1fe2b91ea41d83bb7c
user-ank <ankush1maji@gmail.com>
2023-01-11 16:47:10 +0530

    Removed image warnings

--- src/components/header/CartIcon.js
+++ src/components/header/CartIcon.js
@@ -4,7 +4,7 @@ import classes from './CartIcon.module.css'
 export default function CartIcon() {
   return (
     <div className={classes.iconContainer}>
-        <img className={classes.icon} src={require('../img/cart.png')}/>
+        <img alt='cart icon' className={classes.icon} src={require('../img/cart.png')}/>
         <div className={classes.iconNumber}>2</div>
     </div>
   )
--- src/components/header/WishlistIcon.js
+++ src/components/header/WishlistIcon.js
@@ -6,7 +6,7 @@ import classes from './WishlistIcon.module.css'
 export default function WishlistIcon() {
   return (
     <div className={classes.iconContainer}>
-        <img className={classes.icon} src={require('../img/wishlistIcon.png')}/>
+        <img alt='wishlist icon' className={classes.icon} src={require('../img/wishlistIcon.png')}/>
         Wishlist
     </div>
   )
