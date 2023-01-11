873171d3a6f249ef83bced49883282a4bc67975e
user-ank <ankush1maji@gmail.com>
2023-01-11 19:55:29 +0530

    Home Page added

---
+++ src/components/pages/HomePage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import classes from './HomePage.module.css'
+
+export default function HomePage() {
+  return (
+    <div className={classes.center}>
+      Proceed To  "localhost:3000/products"  to see product list.<br/>
+      Searchbar Doesn't work right now
+    </div>
+  )
+}
