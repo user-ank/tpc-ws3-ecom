98572329cfa6a124a863ab503ae62563c97f0e70
user-ank <ankush1maji@gmail.com>
2023-01-12 18:49:11 +0530

    Changes saved

--- src/App.js
+++ src/App.js
@@ -6,9 +6,20 @@ import Loader from "./components/Loader";
 import ProductPage from "./components/pages/ProductPage";
 import { Routes, Route } from 'react-router-dom';
 import HomePage from "./components/pages/HomePage";
+// import React, { createContext } from "react";
 
+// const Cart = React.createContext();
 
 function App() {
+
+
+	let cartList = ["habibi ", "come ", "to ", "dubai"];
+
+	function poping(){
+		cartList.pop();
+	}
+
+	document.getElementById("cartNumber").innerHTML = cartList.length;
 	return (
 		<>
 			<Header />
@@ -17,8 +28,8 @@ function App() {
 				<Route path="/" exact element={<HomePage/>} />
 				<Route path="/products" element={<DashboardPage />} />
 				<Route path="/wishlist" element={<WishlistPage />} />
-				<Route path="/cart" element={<CartPage />} />
-				<Route path="/products/:productId" element={<ProductPage />}/>
+				<Route path="/cart" element={<CartPage items={cartList} />} />
+				<Route path="/products/:productId" element={<ProductPage onclick={poping}/>}/>
 			</Routes>
 
 
