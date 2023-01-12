584b5a2348218c389cbd18935cfafac41767f8c9
user-ank <ankush1maji@gmail.com>
2023-01-12 23:04:16 +0530

    Updated CartPage

--- src/components/pages/CartPage.js
+++ src/components/pages/CartPage.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import { useState } from 'react'
+import { Link } from 'react-router-dom';
+import classes from '../ProductList.module.css'
+import ProductInCart from '../ProductInCart';
 // import {Cart} from '../../App'
 
 export default function CartPage(props) {
@@ -7,10 +10,27 @@ export default function CartPage(props) {
   const [cartList, updateList] = useState(props.items);
   console.log(cartList);
 
+
+
+
   return (
-    <div>
-      Your Cart
-      
+    <div className={classes.list}>
+      <center>Product List</center>
+      {
+        cartList.map(product => (
+          
+            <Link to={"/products/" + product.id}>
+              <ProductInCart key={product.id}
+                id={product.id}
+                proName={product.title} 
+                image={product.images[0]}
+                proRating={product.rating}
+                proPrice={product.price}
+                discount={product.discountPercentage}
+                />
+            </Link>
+        ))
+      }
     </div>
   )
 }
